e734f594f7dd48ad5862998656953b3f2113b406
Brad Mortensen <b.mort86@gmail.com>
2019-05-20 15:01:09 -0600

    Update dbConfig.js

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,5 +2,5 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 const dbEnv = process.env.DB_ENV || "development";
-const db = knex(config.development);
+const db = knex(config[dbEnv]);
 module.exports = db;
