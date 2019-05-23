fbad7df6616482b0f79bf65089680fec90381603
Brad Mortensen <b.mort86@gmail.com>
2019-05-23 14:30:41 -0600

    checks password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,68 +1,63 @@
-const router = require("express").Router();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const secrets = require("../config/secrets");
-const Users = require("../user/users-model");
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const secrets = require('../config/secrets');
+const Users = require('../user/users-model');
 
 // register
-router.post("/register", async (req, res) => {
-  const user = req.body;
-  try {
-    if (
-      user.name === "" ||
-      user.name === null ||
-      user.email === "" ||
-      user.email === null ||
-      user.password === "" ||
-      user.password === null
-    ) {
-      res.status(400).json({ message: "Please Fill In All the Fields" });
-    } else {
-      const hash = bcrypt.hashSync(user.password, 10);
-      user.password = hash;
-      const addedUser = await Users.addUser(user);
-      res.status(201).json({ message: "Success" });
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.post('/register', async (req, res) => {
+	const user = req.body;
+	try {
+		if (
+			user.name === '' ||
+			user.name === null ||
+			user.email === '' ||
+			user.email === null ||
+			user.password === '' ||
+			user.password === null
+		) {
+			res.status(400).json({ message: 'Please Fill In All the Fields' });
+		} else {
+			const hash = bcrypt.hashSync(user.password, 10);
+			user.password = hash;
+			const addedUser = await Users.addUser(user);
+			res.status(201).json({ message: 'Success' });
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 // login
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    if (
-      email === "" ||
-      email === null ||
-      password === "" ||
-      password === null
-    ) {
-      res
-        .status(401)
-        .json({ message: "Please make sure login credentials are accurate" });
-    } else {
-      const user = await Users.findByEmail(email).first();
-      const token = generateToken(user);
-      res.status(200).json({ message: "Success", token });
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		if (email === '' || email === null || password === '' || password === null) {
+			res.status(401).json({ message: 'Please make sure login credentials are accurate' });
+		} else {
+			const user = await Users.findByEmail(email).first();
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user);
+				res.status(200).json({ message: 'Success', token });
+			}
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 function generateToken(user) {
-  const payload = {
-    id: user.id,
-    name: user.name,
-    email: user.email
-  };
+	const payload = {
+		id: user.id,
+		name: user.name,
+		email: user.email
+	};
 
-  const options = {
-    expiresIn: "1h"
-  };
+	const options = {
+		expiresIn: '1h'
+	};
 
-  return jwt.sign(payload, secrets.jwtSecret, options);
+	return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
 module.exports = router;
