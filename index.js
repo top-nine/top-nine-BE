ee7792c29e60e13d488611c7074a3aa2eb5b46d7
Brad Mortensen <b.mort86@gmail.com>
2019-06-22 21:36:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 5000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
